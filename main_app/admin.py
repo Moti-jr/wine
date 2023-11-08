bc2a6e0b5c06e2e9c6692a5d363d2d54fba04f55
Moti-Jr <kipchimoti@gmail.com>
2023-11-08 13:36:08 +0300

    database

---
+++ main_app/admin.py
@@ -0,0 +1,21 @@
+import email
+
+from django.contrib import admin
+
+from main_app.models import Student
+
+# Register your models here.
+# customize the header
+admin.site.site_header = 'ST Francis Boys'
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'kcpe_score', 'email'
+                    ]
+    # the search functionality
+
+    search_fields = ['first_name'', "kcpe_score', 'is_spoty']
+    list_filter = ["is_spoty"]
+
+
+admin.site.register(Student, StudentAdmin)
