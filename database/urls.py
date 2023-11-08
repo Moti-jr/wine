5d186310b9e494bbafe73bc334d70b0a1fde4e26
Moti-Jr <kipchimoti@gmail.com>
2023-11-08 14:54:44 +0300

    database

--- database/urls.py
+++ database/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main_app import views
+
 urlpatterns = [
+    path('', views.students, name='home'),
+
     path('admin/', admin.site.urls),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from app_forms import StudentForm
+
+
 # Create your views here.
+def students(request):
+    form = StudentForm()
+
+    return render(request, 'students.html',{'form':form})
